f510b56074658375e961f78b12f7f19e8070259b
Алексей <joker198223@gmail.com>
2021-07-14 15:53:12 +0800

    Ex2

--- Example2.py
+++ Example2.py
@@ -1,1 +1,15 @@
-# Поиск самого крупного файла
+# Поиск самого крупного файла с исходным кодом Python в отельно взятом каталоге
+
+import os, glob
+dirname = r'C:\Python38\Lib'
+
+allsizes = []
+allpy = glob.glob(dirname + os.sep + '*.py')
+for filename in allpy:
+    filesize = os.path.getsize(filename)
+    allsizes.append((filesize, filename))
+
+allsizes.sort()
+print(allsizes[:2])
+print(allsize[-2:])
+
