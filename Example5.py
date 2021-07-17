e1792630c973b509fbb9bd23e84f36989ee67ea7
Sibiryak82 <joker198223@gmail.com>
2021-07-17 08:18:57 +0800

    Ex5

---
+++ Example5.py
@@ -0,0 +1,14 @@
+# Пример похож на предыдущий, но для хранения сумм вместо словарей
+# используются списки
+
+import sys
+filename = sys.argv[1]
+numcols = int(sys.argv[2])
+totals = [0] * numcols
+
+for line in open(filename):
+    cols = line.split(',')
+    nums = [int(x) for x in cols]
+    totals = [(x + y) for (x, y) in zip(totals, nums)]
+
+print(totals)
