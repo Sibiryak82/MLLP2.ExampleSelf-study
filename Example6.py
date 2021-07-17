b9077c8870ea5081a37d76faec762e73af49a3c4
Sibiryak82 <joker198223@gmail.com>
2021-07-17 08:42:31 +0800

    Ex6

---
+++ Example6.py
@@ -0,0 +1,18 @@
+# Проверка возвращения в предыдущее состояние вывода для набора сценариев
+
+import os
+testscripts = [dict(script='test1.py', args=''),     # Или глобальный каталог
+                                                     # сценариев и аргументы
+               dict(script='test2.py', args='spam')]
+
+for testcase in testscripts:
+    commandline = '%(script)s %(args)s' % testcase
+    output = os.popen(commandline).read()
+    result = testcase['script'] + '.result'
+    if not os.path.exists(result):
+        open(result, 'w').write(output)
+        print('Created:', result)
+    else:
+        priorresult = open(result).read()
+        if output != priorresult:
+            print('FAILED:', testcase['script'])
